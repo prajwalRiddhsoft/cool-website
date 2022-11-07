1962de76b6de78ccfdff692eb583876610fb8331
Prajwal Gautam <prajwalgautam10@gmail.com>
2022-11-08 00:54:04 +0545

    Better rotation movement

--- src/App.tsx
+++ src/App.tsx
@@ -14,47 +14,55 @@ function App() {
 
   const handleOnClick = () => {
     if(orientation === Orientation.LANDSCAPE){
-      containerElement.current.classList.add("change-img");
-      containerElement.current.style.backgroundSize = "100vh auto";
-      containerElement.current.style.backgroundPosition = "0vw 0vh";
+      containerElement.current.style.top = "-30vw";
+      containerElement.current.style.left = "-100vw";
+      containerElement.current.style.transform = "rotate(90deg)";
       setOrientation(Orientation.PORTRAIT);
     } else {
-      containerElement.current.classList.remove("change-img");
-      containerElement.current.style.backgroundSize =  "100vw auto";
-      containerElement.current.style.backgroundPosition =  "0vw 75vh";
+      containerElement.current.style.top = "50vh";
+      containerElement.current.style.left = "-20vw";
+      containerElement.current.style.transform = "rotate(30deg)";
       setOrientation(Orientation.LANDSCAPE);
     }
   }
   const handleOnHover = () => {
     if(orientation === Orientation.LANDSCAPE){
-      containerElement.current.style.backgroundSize =  "110vw auto";
-      containerElement.current.style.backgroundPosition =  "0vw 45vh";
+      containerElement.current.style.top = "50vh";
+      containerElement.current.style.transform = "rotate(30deg)";
     }
   }
   const handleRemoveHover = () => {
     if(orientation === Orientation.LANDSCAPE){
-      containerElement.current.style.backgroundSize =  "100vw auto";
-      containerElement.current.style.backgroundPosition =  "0vw 75vh";
+      containerElement.current.style.top = "70vh";
+      containerElement.current.style.transform = "rotate(-30deg)";
     }
     
   }
 
   return (
-    <div className="App" ref={containerElement}>
-      {/*Text area*/}
-      <div className='text-container'>
-        <h1>Explore the cosmos</h1>
-        <p>Hello this is bossman, come along with me, see it for yourself</p>
-        <div className='button-section'>
-          <button className='learn-more'>Learn more</button>
-          <button className='explore' 
-            onClick={handleOnClick}
-            onMouseOver={handleOnHover}
-            onMouseLeave={handleRemoveHover}
-          >Explore</button>
+    <div className="App">
+
+      <div className="top-container">
+        <div className='img-area' ref={containerElement}></div>
+
+        <div className='container'>
+          {/*Text area*/}
+          <div className='text-container'>
+            <h1>Explore the cosmos</h1>
+            <p>Hello this is bossman, come along with me, see it for yourself</p>
+            <div className='button-section'>
+              <button className='learn-more'>Learn more</button>
+              <button className='explore' 
+                onClick={handleOnClick}
+                onMouseOver={handleOnHover}
+                onMouseLeave={handleRemoveHover}
+              >Explore</button>
+            </div>
+          </div>
+          {/*Text area*/}
         </div>
       </div>
-      {/*Text area*/}
+      
     </div>
   );
 }
